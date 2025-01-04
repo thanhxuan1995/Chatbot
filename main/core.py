e77b2dbb6912cf9746291e28df8943c282f41399
XuanNguyen <109850737+thanhxuan1995@users.noreply.github.com>
2025-01-04 16:10:44 +0700

    Create core.py
    created the main.py

---
+++ main/core.py
@@ -0,0 +1,9 @@
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from typing import Dict, Any
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+os.environ["HTTP_PROXY"] = os.environ['PROXY_URL']
+os.environ["HTTPS_PROXY"] = os.environ['PROXY_URL']
